4b1b98ad5744995b8411d9ee3156613533290152
Renato S. Oliveira <50246351+W335@users.noreply.github.com>
2019-05-22 15:55:11 -0300

    Update Cadastro_Funcionario.cpp

--- Cadastro_Funcionario.cpp
+++ Cadastro_Funcionario.cpp
@@ -1,41 +1,58 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
 
-struct inclusionFuncionario{
-	char nome[50];
-	int cpf[11];
-	char endereco[40];
-	char cidade[20];
-	double salario;
-	int idade;
-	char cargo[25];
-};
+	struct inclusionFuncionario{
+		char nome[100];
+		int idade;
+		int cpf[11];
+		char cargo[25];
+		char cidade[20];
+		char endereco[100];
+		double salario;
 
-int main (){
-	int escolha, qtdFuncionario=1;
-	struct inclusionFuncionario *funcionarios;
+	};
 	
-	funcionarios = (inclusionFuncionario *) malloc (qtdFuncionario*sizeof(inclusionFuncionario));
-	
-	printf("Adicionar  [1]\n");
-	printf("AlteraÁ„o  [2]\n");
-	printf("Consulta   [3]\n");
-	printf("Exclus„o   [4]\n");
-	printf("Listagem   [5]\n");
-	scanf("%d", &escolha);
-	
-	switch (escolha){
-		case 1: 
-			
-			printf("Digite o nome do funcionario");
-			scanf("%s", &funcionarios[(qtdFuncionario-1)].nome);
-			
-			
-			
-		qtdFuncionario++;	
-		funcionarios = (inclusionFuncionario *)realloc( funcionarios, qtdFuncionario * sizeof(inclusionFuncionario));		
-		break;
+	int main (){
+		setlocale(LC_ALL,"portuguese");
+		int escolha, qtdFuncionario=1;
+		struct inclusionFuncionario *funcionarios;
 		
+		funcionarios = (inclusionFuncionario *) malloc (qtdFuncionario*sizeof(inclusionFuncionario));
+		
+		printf("Adicionar Funcionario              [1]\n");
+		printf("Alterar de dados de um funcionario [2]\n");
+		printf("Consulta[3]\n");
+		printf("Exclus√£o[4]\n");
+		printf("Listagem[5]\n");
+		printf("-----------------------------------------");
+		printf("Escolha: ");
+		scanf("%d", &escolha);
+
+		switch (escolha){
+			case 1:
+				printf("Digite o nome do funcionario: ");
+				fflush(stdin);
+				scanf("%s", &funcionarios[(qtdFuncionario-1)].nome);
+				printf("Insira a idade desse funcionario: ");
+				scanf("%d",&funcionarios[qtdFuncionario - 1].idade);
+				printf("Digite o CPF do funcionario: ");
+				scanf("%d", &funcionarios[qtdFuncionario - 1].cpf);
+				printf("Isira o cargo do funcionario: ");
+				fflush(stdin);
+				scanf("%s",&funcionarios[qtdFuncionario - 1].cargo);
+				printf("Informe a cidade do funcionario: ");
+				fflush(stdin);
+				scanf("%s", &funcionarios[qtdFuncionario - 1].cidade);
+				printf("Escreva o endere√ßo do funcionario: ");
+				fflush(stdin);
+				scanf("%s", &funcionarios[qtdFuncionario - 1].endereco);
+				printf("Informe o quanto esse funcionario recebe: ");
+				scanf("%lf", &funcionarios[qtdFuncionario - 1].salario);
+				qtdFuncionario++;
+				funcionarios = (inclusionFuncionario *)realloc( funcionarios, qtdFuncionario * sizeof(inclusionFuncionario));		
+			break;
+			case 2:
+				
+		}
 	}
-	
-}
